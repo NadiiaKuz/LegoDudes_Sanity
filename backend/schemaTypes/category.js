61101f41c163e1833aaa12fc7a19110c542586b8
NadiiaKuz <nadink1603@gmail.com>
2026-03-30 12:17:11 +0200

    added schemas

---
+++ backend/schemaTypes/category.js
@@ -0,0 +1,11 @@
+const category = {
+    name: "category",
+    title: "Kategori",
+    type: "document",
+    fields: [
+        { name: "categoryname", title: "Kategorinavn", type: "string" },
+        { name: "categoryimage", title: "Kategoribillede", type: "image" }
+    ]
+}
+
+export default category
--- backend/schemaTypes/index.js
+++ backend/schemaTypes/index.js
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import category from "./category";
+import products from "./products";
+
+export const schemaTypes = [category, products]
