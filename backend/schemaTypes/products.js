b31d5409f153c8625995a04c1c9674a7397f6993
NadiiaKuz <nadink1603@gmail.com>
2026-04-10 13:57:13 +0200

    forelesning 10.04

--- backend/schemaTypes/products.js
+++ backend/schemaTypes/products.js
@@ -1,14 +1,43 @@
+import category from "./category"
+
 const products = {
     name: "product",
     title: "Produkter",
     type: "document",
     fields: [
         { name: "productname", title: "Produktnavn", type: "string" },
+        {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source: 'productname',
+                slugify: input => input
+                                    .toLowerCase()
+                                    .replace(/\s+/g, '-') // regular expresions to replace whitespace with dashes
+                                    .slice(0, 100)
+            }
+        },
         { name: "price", title: "Pris", type: "number" },
         { name: "quantity", title: "Antall på lager", type: "number" },
         { name: "productimage", title: "Produktbillede", type: "image" },
         { name: "productcategory", title: "Kategori", type: "reference", to: [{ type: "category" }] }
-    ]
+    ],
+    preview: { // regelset på hvordan inhold skal vises i admin panelet
+        select: {
+            title: "productname",
+            inCat: "productcategory.categoryname",
+            image: "productimage"
+        },
+        prepare(selection) {
+            const {title, inCat, image} = selection
+            return {
+                title: title,
+                subtitle: `Kategori: ${inCat ? inCat : "Ukjent"}`,
+                media: image
+            }
+        }
+    }
 }
 
 export default products
