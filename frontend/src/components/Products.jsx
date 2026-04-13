82aacebfbad488d9ef6b8eb4404c12dd03df4446
NadiiaKuz <nadink1603@gmail.com>
2026-04-13 14:03:36 +0200

    forelesning 13.04

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react'
 import ProductCard from './ProductCard'
+import client from '../helpers/client'
+
 export default function Products({products, setCart}){
-    return (
-    <div id="product-list">
-      {products.map(p => <ProductCard key={p.prodid} p={p} setCart={setCart} />)}
-    </div>)
-  }
+  const [sanityProducts, setSanityProducts] = useState(null)
+
+  useEffect(() => { // for å sørge for at vi kjører denne koden når komponenten lastes inn
+    async function fetchAllProducts() {
+      const allProducts = await client.fetch("*[_type == 'product']{_id, productname, price, 'category': productcategory->categoryname, 'imageURL': productimage.asset->url }") // spør om clienten kan hente alle dokumenter av typen "product"
+      setSanityProducts(allProducts)
+    }
+
+    fetchAllProducts() // kjør funksjonen for å hente produktene
+  }, []) // tom dependency-list betyr at denne useEffect-en kun kjører én gang, når komponenten først lastes inn
+
+  console.log(sanityProducts)
+
+  return (
+  <div id="product-list">
+    {sanityProducts?.map(p => <ProductCard key={p._id} p={p} setCart={setCart} />)}
+  </div>)
+}
